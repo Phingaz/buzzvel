bb66033866724327190cfd527401e1e9a9a84d34
Prosper <piinoya@gmail.com>
2024-10-24 08:49:11 -0300

    bug fix

--- app/_components/sections/Features.tsx
+++ app/_components/sections/Features.tsx
@@ -21,8 +21,8 @@ const Features = () => {
         <motion.div
           className="bg-secondary absolute h-[500px] w-[800px] lg:h-[650px] sm:w-[1000px] rounded-full -z-10 md:-right-[450px] md:top-[100px] sm:-bottom-[1300px] sm:-right-[350px] -bottom-[1530px] -right-[450px] transform rotate-45"
           viewport={{ once: true }}
-          initial={{ scale: 0, opacity: 0, rotate: 45 }}
-          whileInView={{ scale: 1, opacity: 1, rotate: -45 }}
+          initial={{ scale: 0, opacity: 0, rotate: -45 }}
+          whileInView={{ scale: 1, opacity: 1, rotate: 45 }}
           transition={{ duration: 1, delay: 0.2 }}
         ></motion.div>
       </motion.div>
